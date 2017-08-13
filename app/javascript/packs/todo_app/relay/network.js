8d0e1dd9cd38a64bd8537aafa88afcaf92ae45b9
PastLeo <chgu82837@gmail.com>
2017-08-13 13:10:57 +0800

    add docker support
    I just want to run this under my docker env
    here is how I run this:
    
    1. create dockerfiles for rails and webpack_dev container,
    docker-compose.yml
    2. because of docker, we webpack server should bind on 0.0.0.0 but
    public access host should not be 0.0.0.0, so I added an option 'public'
    to dev_server in config/webpacker.yml
    3. use an new option 'allowedHosts' from webpack-dev-server@2.7.1

--- app/javascript/packs/todo_app/relay/network.js
+++ app/javascript/packs/todo_app/relay/network.js
@@ -7,10 +7,13 @@ function fetchQuery(
   cacheConfig,
   uploadables,
 ) {
+  const csrf_token = document.querySelector('meta[name=csrf-token]').getAttribute('content')
   return fetch(API_ENDPOINT, {
     method: 'POST',
+    credentials: 'same-origin',
     headers: {
       'content-type': 'application/json',
+      'X-CSRF-Token': csrf_token,
     },
     body: JSON.stringify({
       query: operation.text,
